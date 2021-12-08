117372c8f7aef61a9f6691ff6dafa780bedd6b0d
shihar <shiharbinsideeque73@gmail.com>
2021-12-08 15:57:52 +0530

    login page created

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    user = request.form["user"]
+    passwd = request.form["password"]
+    print(user,passwd)
+
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
