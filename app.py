853c5b5fc8964119544746bebb0d92dbf162940e
shihar <shiharbinsideeque73@gmail.com>
2021-12-15 14:32:47 +0530

    login setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from helpers import db_user_querys as db_user
 
 
 app = Flask(__name__)
-
+app.secret_key = "dont tell"
 
 
 @app.route('/')
@@ -17,7 +17,11 @@ def login():
     passwd = request.form["password"]
     data = db_user.login(user, passwd)
     print(data)
-    return render_template('home.html')
+    if data['status']:
+        return render_template('home.html')
+    else:
+        flash(data['error_msg'],data['error'])
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(port=6060)
--- helpers/db_user_querys.py
+++ helpers/db_user_querys.py
@@ -5,7 +5,21 @@ from .collections import user_col
 col = db[user_col]
 
 def login(user, passwd):
-    print('db working')
     print(user,passwd)
+    login_status = {
+        'status':False
+    }
+    user_data = [i for i in col.find({"user_id":user})]
+    if user_data:
+        print(user_data[0])
+        if passwd == user_data[0]['password']:
+            login_status['status'] = True
+            return login_status
+        else:
+            login_status['error_msg'] = 'Please Check Password'
+            login_status['error'] = 'passwd'
+    else:
+        login_status['error_msg'] = 'Please Check UserId'
+        login_status['error'] = 'user_id'
 
-    return 'seccess'
+    return login_status 
